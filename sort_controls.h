09f023fb2d5b30ef27a6a82bce35ae97df63bbc3
Stephen C <cranegames@gmail.com>
2018-10-25 11:47:05 -0700

    Updated sort controls to be more friendly

--- sort_controls.h
+++ sort_controls.h
@@ -13,30 +13,32 @@ using namespace std;
 static const bool DEBUG = true;
 //SIGNATURES
 template<class T> //shuffles an array
-T* shuffle(T *arr, const size_t& size);
+void shuffle(T *arr, const size_t& size);
 template<class T>
 void copy_array(T* &copy, const T orig[], const size_t& size);
 template <class T>
 bool verify_inc(T* a, const size_t& size);
 template <class T>
-bool verify_dec(const T a[], const size_t& size);
+bool verify_dec(const T * a, const size_t& size);
 template<class T>
-bool verify_inc_abs(const T a[], const size_t& size);
+bool verify_inc_abs(const T * a, const size_t& size);
 template<class T>
-bool verify_dec_abs(const T a[], const size_t& size);
+bool verify_dec_abs(const T * a, const size_t& size);
+template <class T, class Func>
+string verifystr(Func f, T* a, const size_t& size);
+template<class T>
+bool verify(T* a, const size_t& size, bool asc=true, bool ab=false);
 template <class T>
-string verifystr(function<bool(const T*, const size_t&)> v_func, T* a, const size_t& size);
-template <class T>
-void print_array(const T a[], int size, bool status_only = false);
+void print_array(const T * a, int size, bool status_only = false);
 
-//print a[] from start to end
+//print * a from start to end
 template <class T>
-void print_array_segment(const T a[], const size_t& start, const size_t& end, int interval=1);
+void print_array_segment(const T * a, const size_t& start, const size_t& end, int interval=1);
 
 
 //Sorting array control functions
 template<class T> //shuffles an array
-T* shuffle(T *arr, const size_t& size){
+void shuffle(T *arr, const size_t& size){
     T* shuffled_arr = new T[size];
     for(size_t i = 0; i < size; i++)
         shuffled_arr[i] = arr[i];
@@ -44,7 +46,7 @@ T* shuffle(T *arr, const size_t& size){
     for(size_t i = 0; i < 2*size; i++){
         swap(shuffled_arr[rand()%size], shuffled_arr[rand()%size]);
     }
-    return shuffled_arr;
+    arr = shuffled_arr;
 }
 template<class T>
 void copy_array(T* &copy, const T orig[], const size_t& size){
@@ -63,7 +65,7 @@ bool verify_inc(T* a, const size_t& size){
     return true;
 }
 template <class T>
-bool verify_dec(const T a[], const size_t& size){
+bool verify_dec(const T * a, const size_t& size){
     assert(size >= 2);
     //check that the list is not-increasing
     for(size_t i = 1; i < size; i++){
@@ -73,7 +75,7 @@ bool verify_dec(const T a[], const size_t& size){
     return true;
 }
 template<class T>
-bool verify_inc_abs(const T a[], const size_t& size){
+bool verify_inc_abs(const T * a, const size_t& size){
     assert(size >= 2);
     //check that the list is not-increasing
     for(size_t i = 1; i < size; i++){
@@ -83,7 +85,7 @@ bool verify_inc_abs(const T a[], const size_t& size){
     return true;
 }
 template<class T>
-bool verify_dec_abs(const T a[], const size_t& size){
+bool verify_dec_abs(const T * a, const size_t& size){
     assert(size >= 2);
     //check that the list is not-increasing
     for(size_t i = 1; i < size; i++){
@@ -92,35 +94,39 @@ bool verify_dec_abs(const T a[], const size_t& size){
     }
     return true;
 }
-//return a string "SORTED" or "NOT SORTED" depending if a[] is sorted
-template <class T, class U>
-string verifystr(function<bool(const U*, int)> v_func, T* a, const size_t& size){
-    if(v_func(a, size)) //?????????
+//return a string "SORTED" or "NOT SORTED" depending if * a is sorted
+template <class T, class Func>
+string verifystr(Func f, T* a, const size_t& size){
+    if(f(a, size)) //?????????
         return "SORTED";
     else
         return "NOT SORTED";
 }
 template<class T>
+bool verify(T* a, const size_t& size, bool asc, bool ab){
+    if(asc){
+        if(!ab)
+            return verify_inc(a, size);
+        else return verify_inc_abs(a, size);
+    }else
+        if(!ab)
+            return verify_dec(a, size);
+        else return verify_dec_abs(a, size);
+}
+template<class T>
 T order_asc(T arr[], size_t& l, size_t& r);
 
-void test_stdfunction(function<void(function<bool(int[],int)>)> func){
-    func([](int a[], int s){
-        for(int i = 0; i < s; i++)
-            cout << "a["<<i<<"]: " << a[i] << endl;
-        return true;
-    });
-}
 
-//print the array if status_only is true along with info on sortedness of a[]
+//print the array if status_only is true along with info on sortedness of * a
 template <class T>
-void print_array(const T a[], int size, bool status_only){
+void print_array(const T * a, int size, bool status_only){
     print_array_segment(a, 0, size);
     //print status
 }
 
-//print a[] from start to end
+//print * a from start to end
 template <class T>
-void print_array_segment(const T a[], const size_t& start, const size_t& end, int interval){
+void print_array_segment(const T * a, const size_t& start, const size_t& end, int interval){
     for(size_t i = start; i < end; i+=interval)
         cout << "[" << setw(4) << setfill('0') << i << "]: " << a[i] << endl;
 }
