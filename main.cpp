c357ea79ab48f4028b761091838136a805616fd5
Stephen C <cranegames@gmail.com>
2018-10-27 01:34:08 -0700

    Added a multiple run timer to get an average time

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <time.h>
 #include <functional>
+#include <sorts>
 #include "sort_controls.h"
 #include "mergesort.h"
 #include "random"
@@ -16,20 +17,39 @@ double time_f(T* a, const size_t& s, Func f, bool output=false, ostream& outs=co
 template<typename T>
 T get_average(T* a, const size_t& s);
 //void time_f(T* a, const size_t& s, void(*f)(T*, const size_t)); //wasn't working
+template<typename T, typename Func>
+double time_xf(const size_t& size, const size_t& runs, Func f);
 
 int main()
 {
-    int* arr_n;
-    size_t a_s = 100000;
-    double* t_arr = new double[100];
+    double average;
+    size_t size = 1000, runs = 100, size2 = 10000, size3 = 100000;
+    srand(time(NULL));
+//    //test mergesort
+//    average = time_xf<int>(size2, runs, mergesort<int>);
+//    cout << "Average mergesort for " << size2 << " elements: "
+//         << average << " milliseconds\n";
+//    //test insertionsort
+//    average = time_xf<int>(size, runs, bubblesort<int>);
+//    cout << "Average bubblesort " << size << " elements: "
+//         << average << " milliseconds\n";
+//    //test insertionsort
+//    average = time_xf<int>(size, runs, insertionsort<int>);
+//    cout << "Average insertionsort " << size << " elements: "
+//         << average << " milliseconds\n";
+//    //test quicksort
+//    average = time_xf<int>(size2, runs, quicksort<int>);
+//    cout << "Average quicksort " << size2<< " elements: "
+//         << average << " milliseconds\n";
+    //test heapsort
+    average = time_xf<int>(size, runs, heapsort<int>);
+    cout << "Average heapsort " << size << " elements: "
+         << average << " milliseconds\n";
 
-    for(size_t i = 0; i < 100; i++){
-        arr_n = get_rand_array<int>(a_s, 0, a_s*10);
-        time_f(arr_n, a_s, shuffle<int>);
-        t_arr[i] = time_f(arr_n, a_s, mergesort<int>);
-    }
-//    print_array(t_arr, 100);
-    cout << "Average for mergesort: " << get_average(t_arr, 100) << " milliseconds\n";
+
+//    print_array(arr,10);
+//    insertionsort(arr, 10);
+//    print_array(arr,10);
 
     return 0;
 }
@@ -61,6 +81,20 @@ double time_f(T *a, const size_t& s, Func f, bool output, ostream& outs){
         outs << "The function ran in " << setprecision(3)
              << elapsed << " milliseconds" << endl;
     return elapsed;
+}
+template<typename T, typename Func>
+double time_xf(const size_t &size, const size_t& runs, Func f){
+    size_t a_s = size;
+//    double* t_arr = new double[runs];
+    double time = 0;
+    T* arr_n;
+
+    arr_n = get_rand_array<int>(a_s, 0, a_s*10);
+    for(size_t i = 0; i < runs; i++){
+        shuffle(arr_n, size);
+        time += time_f(arr_n, a_s, f, false);
+    }
+    return time/runs;
 }
 template<typename T>
 T get_average(T* a, const size_t& s){
