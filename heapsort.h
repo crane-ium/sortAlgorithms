7b130871de01eb87728b7148bdcbb2d639124732
Stephen C <cranegames@gmail.com>
2018-10-27 01:34:29 -0700

    initial commit

---
+++ heapsort.h
@@ -0,0 +1,29 @@
+//heapsort -*- C++ -*-
+
+//Copyright (C) 2018-2018 Stephen Crane and Co
+
+//This file is part of the sorting algorithm library.
+//I hereby grant any one who loves my code to use, redistribute, and/or
+//modify my code for personal or commercial use under the
+//Stephen Is Best General LIcense as published by Stephen Crane.
+
+//No guarantees though.
+
+/** @file include/heapsort
+ * Standard C++ header
+ */
+#ifndef HEAPSORT_H
+#define HEAPSORT_H
+
+#include "sort_controls.h"
+
+template<class T>
+void heapsort(T* arr, const size_t& size);
+
+
+template<class T>
+void heapsort(T* arr, const size_t& size){
+
+}
+
+#endif // HEAPSORT_H
