3524f9b12271243bdae553e3083b77c2b1574225
Stephen Crane <cranegames@gmail.com>
2018-10-28 04:29:09 -0700

    Added support tests for all tests up to radix

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include <functional>
 #include <sorts>
+#include <vector>
 #include "sort_controls.h"
 #include "mergesort.h"
 #include "random"
@@ -19,37 +20,36 @@ T get_average(T* a, const size_t& s);
 //void time_f(T* a, const size_t& s, void(*f)(T*, const size_t)); //wasn't working
 template<typename T, typename Func>
 double time_xf(const size_t& size, const size_t& runs, Func f);
+template<class Func>
+void print_int_sort(const size_t& size, const size_t& runs
+                    , Func f, const string& s);
 
 int main()
 {
-    double average;
-    size_t size = 1000, runs = 100, size2 = 10000, size3 = 100000;
+    size_t size = 1000, runs = 3, size2 = 100000, size3 = 100000;
     srand(time(NULL));
 //    //test mergesort
-//    average = time_xf<int>(size2, runs, mergesort<int>);
-//    cout << "Average mergesort for " << size2 << " elements: "
-//         << average << " milliseconds\n";
+    print_int_sort(size2, runs, mergesort<int>, "mergesort");
+//    //test bubblesort
+//    print_int_sort(size, runs, bubblesort<int>, "bubblesort");
 //    //test insertionsort
-//    average = time_xf<int>(size, runs, bubblesort<int>);
-//    cout << "Average bubblesort " << size << " elements: "
-//         << average << " milliseconds\n";
-//    //test insertionsort
-//    average = time_xf<int>(size, runs, insertionsort<int>);
-//    cout << "Average insertionsort " << size << " elements: "
-//         << average << " milliseconds\n";
+//    print_int_sort(size, runs, insertionsort<int>, "insertionsort");
 //    //test quicksort
-//    average = time_xf<int>(size2, runs, quicksort<int>);
-//    cout << "Average quicksort " << size2<< " elements: "
-//         << average << " milliseconds\n";
-    //test heapsort
-    average = time_xf<int>(size, runs, heapsort<int>);
-    cout << "Average heapsort " << size << " elements: "
-         << average << " milliseconds\n";
+    print_int_sort(size2, runs, quicksort<int>, "quicksort");
+//    //test heapsort
+    print_int_sort(size2, runs, heapsort<int>, "heapsort");
+//    //test heaporder: the function that turns an array into heap array
+//    print_int_sort(size2, runs, heaporder_simple<int>, "heaporder-simple");
+//    //test bucketsort
+    print_int_sort(size2, runs, bucketsort<int>, "bucketsort");
 
-
-//    print_array(arr,10);
-//    insertionsort(arr, 10);
-//    print_array(arr,10);
+    print_int_sort(size2, runs, radixsort<int>, "radixsort");
+    size_t s = 100;
+//    int* arr = get_rand_array<int>(s, 0, s*10);
+//    print_array(arr, s);
+//    radixsort(arr, s);
+//    print_array(arr, s);
+//    cout << verifystr(verify_inc<int>, arr, s) << endl;
 
     return 0;
 }
@@ -57,10 +57,13 @@ int main()
 template<typename T>
 T* get_rand_array(const size_t &size, const size_t& min, const size_t& max){
     assert(size>0);
-    srand(time(NULL));
+    cout << "max: " << max << endl;
     T* temp = new T[size];
+    //Form a 32767 x 32767 matrix to generate a larger random int
+    long long randint = rand() * 32767 + rand();
     for(size_t i = 0; i < size; i++){
-        temp[i] = rand() % (max+min) + min;
+        randint = rand() * 32767 + rand();
+        temp[i] = randint % (max+min) + min;
     }
     return temp;
 }
@@ -94,6 +97,7 @@ double time_xf(const size_t &size, const size_t& runs, Func f){
         shuffle(arr_n, size);
         time += time_f(arr_n, a_s, f, false);
     }
+    delete[] arr_n;
     return time/runs;
 }
 template<typename T>
@@ -104,4 +108,26 @@ T get_average(T* a, const size_t& s){
     }
     avg /= s;
     return avg;
+}
+
+template<class Func>
+void print_int_sort(const size_t& size, const size_t& runs
+                    , Func f, const string& s){
+    double average;
+    average = time_xf<int>(size, runs, f);
+    cout << setfill(' ');
+    cout << "| " << left << setw(17) << s << " | Average time for " << setw(9)
+         << size << " elements: " << setiosflags(ios::fixed) << setw(9)
+         << setprecision(3) << right << average << " milliseconds\n";
+    int* arr = get_rand_array<int>(size, 0, size*10);
+    shuffle(arr, size);
+    f(arr, size); //call the sort function
+    //Let's print the first three and last three elements
+    cout << s << " is " << verifystr(verify_inc<int>, arr, size) << endl;
+    for(size_t i = 0; i < 3; i++)
+        cout << "  [" << setfill('0') << setw(log10(size)) << i << "] : "
+             << right << setfill(' ') << setw(log10(size)+1) << arr[i]  << endl;
+    for(size_t i = size-3; i < size; i++)
+        cout << "  [" << setfill('0') << setw(log10(size)) << i << "] : "
+             << right << setfill(' ') << setw(log10(size)+1) << arr[i]  << endl;
 }
